babfbc8e740fb224933f055f359d49a74296de05
SamTeng22 <125368224+SamTeng22@users.noreply.github.com>
2026-05-06 10:02:45 +0800

    Added models

---
+++ src/main/java/com/samantha/ecommerce/model/Category.java
@@ -0,0 +1,20 @@
+package com.samantha.ecommerce.model
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name= "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=True)
+    private String name;
+
+    @OneToMany(mappedBy="category", cascade=CascadeType.ALL)
+    private List<Product> products;
+}
